35bad5eddba1ae8317e8e0abfe742c3fb491099e
Paula De Souza Biazotto <pde-souz@c2r9s8.42lisboa.com>
2023-05-10 17:44:29 +0100

    feat: update moves with push

--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: pde-souz <pde-souz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:50:58 by pde-souz          #+#    #+#             */
-/*   Updated: 2023/05/10 15:59:26 by pde-souz         ###   ########.fr       */
+/*   Updated: 2023/05/10 17:38:30 by pde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	swap(t_list **lst, char type)
 {
 	t_list	*temp;
 
-	// if (*lst == NULL || (*lst)->next == NULL)
-	// 	return ;
+	if (*lst == NULL || (*lst)->next == NULL)
+		return ;
 	temp = *lst;
 	*lst = (*lst)->next;
 	temp->next = temp->next->next;
@@ -26,4 +26,20 @@ void	swap(t_list **lst, char type)
 		ft_printf("sa\n");
 	else if (type == 'b')
 		ft_printf("sb\n");
+}
+
+void	push(t_list **list_1, t_list **list_2, char type)
+{
+	t_list	*temp;
+
+	if (list_1 == NULL || list_2 == NULL)
+		return ;
+	temp = *list_2;
+	*list_2 = (*list_2)->next;
+	temp->next = *list_1;
+	*list_1 = temp;
+	if (type == 'a')
+		ft_printf("pa\n");
+	else if (type == 'b')
+		ft_printf("pb\n");
 }
