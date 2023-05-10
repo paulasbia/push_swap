350d96e2458b10d8720f5255e0e8e51559a8ae8e
Paula De Souza Biazotto <pde-souz@c2r9s8.42lisboa.com>
2023-05-10 16:00:56 +0100

    feat: update moves.c with limits

--- Makefile
+++ Makefile
@@ -28,7 +28,7 @@ check:
 	norminette $(SRCS)
 
 test: $(LIBFTA)
-	gcc -ggdb $(FLAGS) tests/test.c swap.c tests/unity.c $(LIBFTA) -o result.out
+	gcc -ggdb $(FLAGS) tests/test.c moves.c tests/unity.c $(LIBFTA) -o result.out
 
 run: clean fclean test
 	./result.out
--- moves.c
+++ moves.c
@@ -6,7 +6,7 @@
 /*   By: pde-souz <pde-souz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:50:58 by pde-souz          #+#    #+#             */
-/*   Updated: 2023/05/10 15:51:01 by pde-souz         ###   ########.fr       */
+/*   Updated: 2023/05/10 15:59:26 by pde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	swap(t_list **lst, char type)
 {
 	t_list	*temp;
 
+	// if (*lst == NULL || (*lst)->next == NULL)
+	// 	return ;
 	temp = *lst;
 	*lst = (*lst)->next;
 	temp->next = temp->next->next;
