e4fc7e6c4649f3d36c13b35ebc0b77083c2b8192
paulabiazotto <pds_paula@hotmail.com>
2023-06-20 15:36:48 +0100

    feat: refatoring cases_ba

--- cases_utils_ba.c
+++ cases_utils_ba.c
@@ -6,7 +6,7 @@
 /*   By: pde-souz <pde-souz@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:29:43 by pde-souz          #+#    #+#             */
-/*   Updated: 2023/06/20 15:05:37 by pde-souz         ###   ########.fr       */
+/*   Updated: 2023/06/20 15:31:05 by pde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,40 @@
 int	ft_case_rrarrb_a(t_list *a, t_list *b, int nbr)
 {
 	int	i;
+	int	place_a;
+	int	index_b;
 
 	i = 0;
-	if (ft_find_place_a(a, nbr) != 0)
-		i = ft_lstsize(a) - ft_find_place_a(a, nbr);
-	if ((i < (ft_lstsize(b) - ft_find_index(b, nbr))) && ft_find_index(b, nbr))
-		i = ft_lstsize(b) - ft_find_index(b, nbr);
+	place_a = ft_find_place_a(a, nbr);
+	index_b = ft_find_index(b, nbr);
+	if (place_a != 0)
+		i = ft_lstsize(a) - place_a;
+	if ((i < (ft_lstsize(b) - index_b)) && index_b)
+		i = ft_lstsize(b) - index_b;
 	return (i);
 }
 
 int	ft_case_rarb_a(t_list *a, t_list *b, int nbr)
 {
 	int	i;
+	int	index_b;
 
 	i = ft_find_place_a(a, nbr);
-	if (i < ft_find_index(b, nbr))
-		i = ft_find_index(b, nbr);
+	index_b = ft_find_index(b, nbr);
+	if (i < index_b)
+		i = index_b;
 	return (i);
 }
 
 int	ft_case_rrarb_a(t_list *a, t_list *b, int nbr)
 {
 	int	i;
+	int	place_a;
 
 	i = 0;
-	if (ft_find_place_a(a, nbr) != 0)
-		i = ft_lstsize(a) - ft_find_place_a(a, nbr);
+	place_a = ft_find_place_a(a, nbr);
+	if (place_a != 0)
+		i = ft_lstsize(a) - place_a;
 	i = ft_find_index(b, nbr) + i;
 	return (i);
 }
@@ -48,10 +56,12 @@ int	ft_case_rrarb_a(t_list *a, t_list *b, int nbr)
 int	ft_case_rarrb_a(t_list *a, t_list *b, int nbr)
 {
 	int	i;
+	int	index_b;
 
 	i = 0;
-	if (ft_find_index(b, nbr) != 0)
-		i = ft_lstsize(b) - ft_find_index(b, nbr);
+	index_b = ft_find_index(b, nbr);
+	if (index_b != 0)
+		i = ft_lstsize(b) - index_b;
 	i = ft_find_place_a(a, nbr) + i;
 	return (i);
 }
